b0df9ef5c3f1778158099b530bc070e2af40707e
oceane <oceane.rousseeuw@gmail.com>
2019-03-15 15:17:24 +0100

    navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="navbar">
+        <a href="#">Accueil</a>
+        <a href="#">Mes listes</a>
+        <a href="#">À propos</a>
+    </div>
+</template>
+
+<style>
+    #navbar{
+        background-color: black;
+    }
+
+    #navbar a {
+        color : white;
+        display : inline-block;
+        margin: 20px;
+        text-decoration: none;
+    }
+
+    #navbar a:hover{
+        text-decoration: underline;
+    }
+</style>
